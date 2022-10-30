b580860d69e16857f0c21630ae01170281a13b4a
Anton <anton@sin.sk>
2022-10-30 10:50:36 +0100

    1 - 28

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
-<?php
-echo '123';
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Моя первая программа</title>
+		<!-- <script>
+			alert('Привет, мир!');
+		</script> -->
+		<script src="script.js?v=1"></script>
+		<!-- <script src="script1.js"></script>
+		<script src="script2.js"></script> -->
+	</head>
+	<body>
+    <?php
+        echo '<a href="/">back</a><br>';
+    ?>
+		моя первая программа
+	</body>
+</html>
---
+++ script.js
@@ -0,0 +1,86 @@
+"use strict";
+//29
+
+
+//27
+/* let str = 'abcde';
+alert(str[0] + str[2] + str[4]);
+
+let str1 = str[4] + str[3] + str[2] + str[1] + str[0];
+alert(str1);
+
+let num = 3;
+alert(str[num]);
+ */
+//24
+/* let a = 1;
+let b = 2;
+alert(String(a) + String(b));
+
+let aa = 1234567890;
+alert(String(aa).length);
+
+let num1 = 1111;
+let num2 = 2222;
+alert(String(num1).length + String(num2).length);
+ */
+//23
+/* let a = '5px';
+let b = '6px';
+alert(parseInt(a) + parseInt(b));
+
+let aa = '5.5px';
+let bb = '6.25px';
+alert(parseFloat(aa) + parseFloat(bb) + 'px');
+ */
+//22
+/* let a = '10';
+let b = '20';
+alert(Number(a) + Number(b));
+ */
+//20
+/* const PI = 3.14;
+let r = 10;
+alert(2 * PI * r);
+ */
+//19
+/* let a = 'asd';
+let b = 0.5;
+let c = 134;
+console.log(a, b, c);
+alert(eeee);
+ */
+//18
+/* let a;
+alert(a);
+let b = null;
+alert(b);
+let t = true;
+alert(t);
+let f = false;
+alert(f);
+let asd = 'asd';
+let qwe = 'qwe';
+alert(asd * qwe);
+alert(10 / 0);
+alert(-10 / 0);
+ */
+//17
+/* let str1 = 'xxx';
+let str2 = 'yyy';
+let txt = `aaa ${str1} bbb ${str2} ccc`;
+alert(txt);
+let str = `a
+b
+c`;
+alert(str);
+ */
+/* alert('script');
+let num;
+num = 123;
+alert(num); */
+//16
+/* 
+let str = 'javascript';
+alert(str.length);
+ */
